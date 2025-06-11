ff9427e6d2e43862ed5695c1d47845c8aff16dda
gumistan <mariajoseoo@ufps.edu.co>
2025-06-11 16:23:29 -0500

    Implementación de login y registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-// App.jsx
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+
 
 function App() {
-  return <HomePage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+       
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
